2f2e4363a4b82689c699c7cad7dc66b43f4d32d8
Lauriane MD <dmbagdjelauriane@gmail.com>
2023-08-23 22:41:42 +0000

    ajout de data

---
+++ main.py
@@ -0,0 +1,3 @@
+from dataframe0 import deploy
+df=deploy()
+df.head()
---
+++ script.py
@@ -0,0 +1,20 @@
+import requests 
+import pandas as pd
+# import numpy as np
+
+def download_data(url,dataset='./data.csv'):
+   reponse=requests.get(url)
+   with open(dataset, 'wb') as file:
+      file.write(reponse.content)
+      
+# def check_data_quality(dataset):
+#    df=pd.read_csv(dataset)
+#    numeric_values=df.select_dtypes(include=['int64','float64']).columns
+#    categoric_values=df.select_dtypes(include=['object']).columns
+#    #Enlever  les doublons
+#    df.drop_duplicates(inplace=True)
+
+#    #Traitement des valeurs manquantes
+#    df.isna().sum()
+   
+   
