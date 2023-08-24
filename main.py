18262a7e9479733012a28930911ca16c4efb2854
Lauriane MD <dmbagdjelauriane@gmail.com>
2023-08-24 15:54:09 +0000

    ajout de downloaqd_data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from script import *
 url="https://data.humdata.org/dataset/955ab54d-7369-441b-8d39-a2cdb14a14a1/resource/46940d42-d135-4837-823a-50cd3097c8fe/download/millennium_development_goals_mdgs_indicators_sen.csv"
-download_data(url,dataset='data_file.csv')
+dataset='data_file.csv'
+download_data(url,dataset)
 
 
 # from dataframe0 import deploy
